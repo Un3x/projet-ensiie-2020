ccb5f755a7b130b454a3b97bcda032b72bbbbe7d
Thomas Comes <thomas.comes@matters.tech>
2020-02-27 18:53:48 +0100

    Init of the repository

---
+++ Makefile
@@ -0,0 +1,2 @@
+start:
+	php -S localhost:8080 -t public/
---
+++ public/index.php
@@ -0,0 +1,67 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Projet web Ensiie</title>
+    <meta name="description" content="Projet web Ensiie">
+    <meta name="author" content="Thomas COMES">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Projet Web Ensiie 2020</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1>User List</h1>
+        </div>
+        <div class="col-sm-12">
+            <table class="table">
+                <tr>
+                    <th>id</th>
+                    <th>username</th>
+                    <th>email</th>
+                    <th>creation date</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>unex</td>
+                    <td>patati@patata.com</td>
+                    <td>2012/01/01 12:00:00</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>unex</td>
+                    <td>patati@patata.com</td>
+                    <td>2012/01/01 12:00:00</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>unex</td>
+                    <td>patati@patata.com</td>
+                    <td>2012/01/01 12:00:00</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>unex</td>
+                    <td>patati@patata.com</td>
+                    <td>2012/01/01 12:00:00</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="js/scripts.js"></script>
+</body>
+</html>
