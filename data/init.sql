a8f90b3583247bb0fc3bc302c7b0fe0fd1108db0
Hugo_Malard <malard.hugo@gmail.com>
2020-03-24 16:49:28 +0100

    debut de la BD

--- data/init.sql
+++ data/init.sql
@@ -5,6 +5,26 @@ CREATE TABLE "user" (
     created_at TIMESTAMP WITH TIME ZONE
 );
 
+
+CREATE TABLE "Association"(
+	Id_Assoc VARCHAR PRIMARY KEY,
+);
+
+CREATE TABLE "Reunion"(
+	Id_Assoc VARCHAR NOT NULL,
+	Id_reu VARCHAR PRIMARY KEY,
+	Date_reu date NOT NULL,
+	Horaire TIMESTAMP
+);
+
+CREATE TABLE "Participer"(
+	
+);
+
+
+
+
+
 INSERT INTO "user" (username, email, created_at)  VALUES ('unex', 'patati@patata.com', NOW());
 INSERT INTO "user" (username, email, created_at)  VALUES ('caillou', 'caillou@rocher.com', NOW());
 INSERT INTO "user" (username, email, created_at)  VALUES ('viteira', 'vivi@taira.com', NOW());
