1cd87c7519e9cbde7e8f806e8359ae98fccae402
Hugo_Malard <malard.hugo@gmail.com>
2020-03-31 14:34:34 +0200

    Modif'

--- data/init.sql
+++ data/init.sql
@@ -1,18 +1,17 @@
-CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE user(
+    id VARCHAR PRIMARY KEY,
     username VARCHAR NOT NULL,
     email VARCHAR NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE
 );
 
-
-CREATE TABLE Association(
-	Id_Assoc VARCHAR PRIMARY KEY,
-);
-
 CREATE TABLE Administrateur(
 	Id_MembreA VARCHAR,
-	FOREIGN KEY (Id_MembreA) REFERENCES user(id),
+	FOREIGN KEY (Id_MembreA) REFERENCES user(id)
+);
+
+CREATE TABLE Association(
+	Id_Assoc VARCHAR PRIMARY KEY
 );
 
 CREATE TABLE Reunion(
@@ -21,29 +20,33 @@ CREATE TABLE Reunion(
 	Date_reu date NOT NULL,
 	Horaire TIMESTAMP,
 	Id_MembreA VARCHAR,
-	FOREIGN KEY (Id_MembreA) REFERENCES Administrateur(Id_MembreA),
+	FOREIGN KEY (Id_MembreA) REFERENCES Administrateur(Id_MembreA)
 );
 
 CREATE TABLE Appartenir(
 	Id_Assoc VARCHAR,
 	Id_Membre VARCHAR,
 	FOREIGN KEY (Id_Assoc) REFERENCES Association(Id_Assoc),
-	FOREIGN KEY (Id_Membre) REFERENCES user(id),
+	FOREIGN KEY (Id_Membre) REFERENCES user(id)
 );
 
 CREATE TABLE Administrer(
 	Id_Assoc VARCHAR,
 	Id_Membre VARCHAR,
 	FOREIGN KEY (Id_Assoc) REFERENCES Association(Id_Assoc),
-	FOREIGN KEY (Id_Membre) REFERENCES user(id),
+	FOREIGN KEY (Id_Membre) REFERENCES user(id)
 );
 
 CREATE TABLE Participations(
 	Id_reu VARCHAR,
 	Id_Membre VARCHAR,
 	FOREIGN KEY (Id_reu) REFERENCES Reunion(Id_reu),
-	FOREIGN KEY (Id_Membre) REFERENCES user(id),
+	FOREIGN KEY (Id_Membre) REFERENCES user(id)
 );
+
+create view Vue_admin as select(Administrateur.Id_MembresA) 
+	from user 
+	join Administration on (user.id = Administrateur.Id_MembresA);
 
 INSERT INTO "user" (username, email, created_at)  VALUES ('unex', 'patati@patata.com', NOW());
 INSERT INTO "user" (username, email, created_at)  VALUES ('caillou', 'caillou@rocher.com', NOW());
