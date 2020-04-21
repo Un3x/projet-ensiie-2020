97795576227ec84f3e5d10c80e0d6b08a18969fb
Elliu <goyard.louis@gmail.com>
2020-04-21 18:16:04 +0200

    Implementing searchKara()

--- data/init.sql
+++ data/init.sql
@@ -11,7 +11,7 @@ INSERT INTO "user" (username, email, created_at)  VALUES ('viteira', 'vivi@taira
 INSERT INTO "user" (username, email, created_at)  VALUES ('césar', 'jule@cesar.com', NOW());
 INSERT INTO "user" (username, email, created_at)  VALUES ('gengis', 'gengis@khan.com', NOW());
 
-CREATE TABLE IF NOT EXISTS "kara" (
+CREATE TABLE IF NOT EXISTS "karas" (
     id INTEGER PRIMARY KEY,
     song_name   TEXT    NOT NULL,
     source_name TEXT    NOT NULL,
@@ -23,30 +23,31 @@ CREATE TABLE IF NOT EXISTS "kara" (
     author_name TEXT
 );
 
-INSERT INTO "kara" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (1,'GUNDAM WING italian','Gundam Wing','OP','va',1,'it',0,'Viieux');
-INSERT INTO "kara" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (2,'Something','Nadia il Mistero della Pietra Azzura','MV','va',1,'it',0,'Viieux');
-INSERT INTO "kara" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (3,'version italienne','Maple Town Un Nido di Simpatia','OP','va',1,'it',0,'Viieux');
-INSERT INTO "kara" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (4,'God Knows (russian)','Suzumiya Haruhi no Yuuutsu','IS','va',1,'ru',0,'Colgate');
-INSERT INTO "kara" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (5,'Wu Ya','Hitori no Shita The Outcast S2','OP','va',1,'ch',0,'Sting');
-INSERT INTO "kara" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (6,'Xin Yang','Quan Zhi Gao Shou','OP','va',1,'ch',0,'Sting');
-INSERT INTO "kara" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (7,'Franklin','Franklin','OP','va',1,'fr',0,'Phoko');
-INSERT INTO "kara" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (8,'Pegasus Fantasy (VF)','Saint Seiya','OP','va',1,'fr',0,'Viieux');
-INSERT INTO "kara" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (9,'Opening','Sailor Moon','OP','va',1,'fr',0,'Viieux');
+
+INSERT INTO "karas" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (1,'GUNDAM WING italian','Gundam Wing','OP','va',1,'it',0,'Viieux');
+INSERT INTO "karas" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (2,'Something','Nadia il Mistero della Pietra Azzura','MV','va',1,'it',0,'Viieux');
+INSERT INTO "karas" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (3,'version italienne','Maple Town Un Nido di Simpatia','OP','va',1,'it',0,'Viieux');
+INSERT INTO "karas" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (4,'God Knows (russian)','Suzumiya Haruhi no Yuuutsu','IS','va',1,'ru',0,'Colgate');
+INSERT INTO "karas" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (5,'Wu Ya','Hitori no Shita The Outcast S2','OP','va',1,'ch',0,'Sting');
+INSERT INTO "karas" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (6,'Xin Yang','Quan Zhi Gao Shou','OP','va',1,'ch',0,'Sting');
+INSERT INTO "karas" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (7,'Franklin','Franklin','OP','va',1,'fr',0,'Phoko');
+INSERT INTO "karas" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (8,'Pegasus Fantasy (VF)','Saint Seiya','OP','va',1,'fr',0,'Viieux');
+INSERT INTO "karas" (id, song_name, source_name, category, song_type, song_number, language, is_new, author_name) VALUES (9,'Opening','Sailor Moon','OP','va',1,'fr',0,'Viieux');
 
 CREATE TABLE IF NOT EXISTS "queue" (
     position INTEGER PRIMARY KEY,
-    id INTEGER NOT NULL REFERENCES kara
+    id INTEGER NOT NULL REFERENCES karas
 );
 
 CREATE TABLE IF NOT EXISTS "playlist" (
     id INTEGER PRIMARY KEY,
     nom TEXT NOT NULL,
-    createur SERIAL REFERENCES user,
-    content id[] REFERENCES kara
+    createur SERIAL REFERENCES "user",
+    content id[] REFERENCES karas
 );
 
 CREATE TABLE IF NOT EXISTS "lector"(
-    id INTEGER PRIMARY KEY REFERENCES kara,
+    id INTEGER PRIMARY KEY REFERENCES karas,
     ip VARCHAR,
     port INTEGER
 );
---
+++ public/inc/searchKara.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+session_start();
+
+require_once '../../src/Factory/DbAdaperFactory.php';
+
+
+function searchKara(string $str)
+{
+    $dbAdaper = (new DbAdaperFactoryDepth())->createService();
+    $words = explode(" ", $str);
+    $regex = "'.*";
+    foreach ($words as $word)
+    {
+        $regex = $regex . $word . ".*|.*";
+    }
+    $regex = substr($regex, 0, -3);
+    $regex = $regex . "'";
+
+    $req = 'SELECT DISTINCT song_name, song_type, song_number, source_name FROM "kara" WHERE CONCAT_WS(\'||\', song_name, source_name, category) ~* ' . $regex . ';';
+    $ret = $dbAdaper->query($req)->fetch(PDO::FETCH_NUM);
+
+    return $ret;
+}
+
+$results = searchKara("some il");
+foreach ( $results as $result )
+{
+    echo $result . " - ";
+}
+
+?>
