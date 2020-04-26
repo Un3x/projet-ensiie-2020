25c07d77647b1ba6ab343c6820824286ccf3aa53
lec <alec.sadler@ensiie.fr>
2020-04-26 22:49:55 +0200

    added modifyUserCosmetics and titles (amuzévou)

--- public/inc/modifyUserCosmetics.php
+++ public/inc/modifyUserCosmetics.php
@@ -1,10 +1,48 @@
 <?php
 session_start();
-//******
-//This file will allow the user to change his cosmetics.
-//These might be profile picture, title, maybe even his waifu (◡‿◡✿)
-//
-//
+/******/
+/*This file will allow the user to change his cosmetics.
+/*These might be profile picture, title, maybe even his waifu (◡‿◡✿)
+/*
+ * the POST variables this files expects are 'userID', 'newImage' and 'newTitle' (soon newWaifu ??!!?!) 
+/******/
 
+use User\UserRepository;
 
+include '../../src/User.php';
+include '../../src/UserRepository.php';
+include '../../src/Factory/DbAdaperFactory.php';
+
+$dbAdapter = (new DbAdaperFactoryDepth())->createService();
+$userRepository = new UserRepository($dbAdaper);
+
+$userID=htmlspecialchars($_POST['userID']);
+
+if (isset($_POST['newImage'])){
+	try {
+		$sql='UPDATE "userCosmetics" SET IDimage= :newImage WHERE id= :userID;';
+		$stmt=dbAdapter->prepare($sql))
+		$stmt->bindParam('newImage',$_POST['newImage']);
+		$stmt->bindParam('userID',$userID);
+		$stmt->execute();
+	}
+	catch (PDOException $err){
+		header('Location: ../index.php?error=sqlerror');
+		exit();		
+	}	
+}
+
+if (isset($_POST['newTitle'])){
+	try {
+		$sql='UPDATE "userCosmetics" SET IDtitle= :newTitle WHERE id= :userID;';
+		$stmt=dbAdapter->prepare($sql))
+		$stmt->bindParam('newTitle',$_POST['newImage']);
+		$stmt->bindParam('userID',$userID);
+		$stmt->execute();
+	}
+	catch (PDOException $err){
+		header('Location: ../index.php?error=sqlerror');
+		exit();		
+	}	
+}
 ?>
--- public/inc/readMeInc.txt
+++ public/inc/readMeInc.txt
@@ -4,4 +4,4 @@ here lies the files called by the different html pages,
 IF I HAD ONE
 
 
-don't forget to chmod 611 everything here, we don't want people to do anyhing stupid :P
+don't forget to chmod 611 everything here, we don't want people to do anything stupid :P
