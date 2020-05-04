d0bd773941f7765049999baf78f44be1decdaa35
Elliu <goyard.louis@gmail.com>
2020-05-04 15:47:47 +0200

    DeleteKara now sends the command to lektors

--- public/Forms/deleteKara.php
+++ public/Forms/deleteKara.php
@@ -18,8 +18,7 @@ if (isset($_SESSION['id']) && $_SESSION['rights'] === 1)
     $addition->execute();
 
 
-    /*
-    $msg = "add id://" . $_POST['id'] . "\n" ;
+    $msg = "deleteid " . $_POST['id'] . "\n" ;
     error_log("SOCKETS : Starting sending to all lectors");
     send_to_all_lectors($msg);
     error_log("SOCKETS : Finished sending to all lectors");
