60ab3e083e809e76b19135bb3b3a6953d19ed27e
Elliu <goyard.louis@gmail.com>
2020-05-05 16:01:55 +0200

    Upgrading / Downgrading of people now operational

--- src/Users/UserRepository.php
+++ src/Users/UserRepository.php
@@ -16,7 +16,9 @@ class UserRepository
 
     public function fetchAll()
     {
-        $usersData = $this->dbAdapter->query('SELECT * FROM "user"');
+        $usersData = $this->dbAdapter->query(
+            'SELECT * FROM "user"
+            ORDER BY rights DESC, id ASC');
         $users = [];
         foreach ($usersData as $usersDatum) {
             $user = new User();
@@ -35,40 +37,40 @@ class UserRepository
     //check if username is already used , returns 0 if the userName is free   
     public function checkUser ($userName)
     {
-	$users= $this->dbAdapter->prepare('SELECT COUNT(*) FROM "user" WHERE username= :userName');	    
-	$users->execute(['userName'=>$userName]);
-	return $users->fetchColumn();
+    $users= $this->dbAdapter->prepare('SELECT COUNT(*) FROM "user" WHERE username= :userName');     
+    $users->execute(['userName'=>$userName]);
+    return $users->fetchColumn();
     }
 
 
     //check if username is already used , returns 0 if the email is free
     public function checkEmail($email)
     {
-	$emails= $this->dbAdapter->prepare('SELECT COUNT(*) FROM "user" WHERE email= :Email');	    
-	$emails->execute(['Email'=>$email]);
-	return $emails->fetchColumn();
+    $emails= $this->dbAdapter->prepare('SELECT COUNT(*) FROM "user" WHERE email= :Email');      
+    $emails->execute(['Email'=>$email]);
+    return $emails->fetchColumn();
     }
     
     //add a new row to the table "user" and "userCosmetics"
     public function add ($username, $email, $password)
     {
-	$newUser=$this->dbAdapter->prepare('INSERT INTO "user" (username, email, password, rights, created_at) VALUES (:userName, :Email, :passWord, 0, NOW())');
+    $newUser=$this->dbAdapter->prepare('INSERT INTO "user" (username, email, password, rights, created_at) VALUES (:userName, :Email, :passWord, 0, NOW())');
         $newUser->bindParam('userName', $username);
         $newUser->bindParam('Email', $email);
-	$newUser->bindParam('passWord', $password);
-	$newUser->execute();
+    $newUser->bindParam('passWord', $password);
+    $newUser->execute();
 
-	//add a row to userCosmetics
-	$sql='SELECT id FROM "user" WHERE username= :userName';
-	$newUserID=$this->dbAdapter->prepare($sql);
-	$UserRow=$newUserID->execute(['userName'=>$username]);
-	$UserRow=$newUserID->fetch();
-	$id=$UserRow['id'];
+    //add a row to userCosmetics
+    $sql='SELECT id FROM "user" WHERE username= :userName';
+    $newUserID=$this->dbAdapter->prepare($sql);
+    $UserRow=$newUserID->execute(['userName'=>$username]);
+    $UserRow=$newUserID->fetch();
+    $id=$UserRow['id'];
 
-	$newCosmetics='INSERT INTO "userCosmetics" (id, IDimage, IDtitle) VALUES (:ID, 1, 1)';
-	$stmt=$this->dbAdapter->prepare($newCosmetics);
-	$stmt->bindParam('ID', $id);
-	$stmt->execute();
+    $newCosmetics='INSERT INTO "userCosmetics" (id, IDimage, IDtitle) VALUES (:ID, 1, 1)';
+    $stmt=$this->dbAdapter->prepare($newCosmetics);
+    $stmt->bindParam('ID', $id);
+    $stmt->execute();
     }
 
 
@@ -76,9 +78,35 @@ class UserRepository
     {
         $stmt = $this
             ->dbAdapter
-	    ->prepare('DELETE FROM "user" where id = :userId');
+        ->prepare('DELETE FROM "user" where id = :userId');
 
         $stmt->bindParam('userId', $userId);
         $stmt->execute();
     }
+
+
+    public function setRights ($userId, $rights)
+    {
+        $stmt = $this
+            ->dbAdapter
+            ->prepare(
+                'UPDATE "user" SET rights=:rights
+                WHERE id=:id');
+        $stmt->bindParam('rights', $rights);
+        $stmt->bindParam('id', $userId);
+        $stmt->execute();
+    }
+
+
+    public function getRights($id)
+    {
+        $rights = $this
+            ->dbAdapter
+            ->prepare(
+                'SELECT rights FROM "user"
+                WHERE id=:id');      
+    $rights->execute(['id'=>$id]);
+    return $rights->fetchColumn();
+    }
 }
+
