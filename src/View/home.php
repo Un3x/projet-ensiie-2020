816cb146403a75c5e26aa8bac504c3959b11d2b4
Nina Buis <nina_buis@VM-Nina.Buis>
2020-05-06 11:45:58 +0200

    nina_b : Déroulement d'une histoire + Mise à jour de la base de données · Affichage des pages · Gestion des choix · Début et fin d'une histoire

--- src/View/home.php
+++ src/View/home.php
@@ -1,3 +1,8 @@
-<p> Bienvenue sur Webseria courageux voyageur !</p>
+<p> Bienvenue sur Webseria, courageux voyageur !</p>
 
-<p>Ici, t'attendent de nombreuses aventures dont tu es le héros.</p>
+<p>Ici t'attendent de nombreuses aventures dont tu auras la chance d'être le héros.
+</p>
+
+<!-- Lien temporaire pour accéder à La Cité des Voleurs (Hail) -->
+<a href="../../cite_des_voleurs.php">Commencer <strong>La Cité des Voleurs
+                                            </strong></a>
