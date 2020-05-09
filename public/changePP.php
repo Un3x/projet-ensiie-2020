4e192751cdce0a444c7334e7a800e3072d434125
Kiwi5 <kiwi@LAPTOP-D4ADFV9F.localdomain>
2020-05-09 19:39:28 +0200

    Des pictures et du style

--- public/changePP.php
+++ public/changePP.php
@@ -13,10 +13,13 @@ echo "<label for=\"newTitle\">Titre : </label>";
 echo "<select id=\"newTitle\" name=\"cars\">";
 choiceTitleDummy();
 echo "</select><br>";
- for ($i = 1; $i<=16; $i++){
-choicePP("waifu".$i.".png", 200,200,20);
-if($i%4 == 0){echo "<br>";}
- }
+for ($i = 1; $i<=16; $i++){
+	echo '<span class="container">';
+	choicePP("waifu".$i.".png", 200,200,20);
+	echo '<span class="checkmark"></span>';
+	echo "</span>";	
+	if($i%4 == 0){echo "<br>";}
+ 	}
 	
 
 echo "<input class=\"subPP\" type=\"submit\"  value=\"Appliquer les changements\">";
--- src/ViewPictures.php
+++ src/ViewPictures.php
@@ -18,7 +18,7 @@ function choiceTitle(){
 	include 'Factory/DbAdaperFactory.php';
 	$dbAdaper = (new DbAdaperFactory())->createService();
 	$titles=$dbAdaper->prepare('SELECT title FROM "title"');
-	$titles=$dbAdaper->execute();
+	$titles->$execute();
 	foreach($titles as $title){
 	       echo "<option value=\"".$title['title']."\">".$title['title']."</option>";
 	}       
