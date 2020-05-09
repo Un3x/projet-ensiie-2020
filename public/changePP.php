42d7e325f360f2731940b7e12c42c4d1307fb310
Kiwi5 <kiwi@LAPTOP-D4ADFV9F.localdomain>
2020-05-09 17:12:00 +0200

    few changes

--- public/changePP.php
+++ public/changePP.php
@@ -1,18 +1,21 @@
 <?php session_start();
 set_include_path('.:'.$_SERVER['DOCUMENT_ROOT'].'/../src');?>
-
+<?php $_SESSION["username"] = "poupou";
+?>
 <?php include_once "../src/ViewPictures.php"?>
 
 <?php include_once "View/Layout/head.php" ?>
 <?php include_once "View/Layout/header.php"?>
 
 <?php if (isset($_SESSION['username'])){
-
+echo "<form action=\"/Forms/modifyUserCosmetics.php\" method=\"post\">";
  for ($i = 1; $i<=16; $i++){
 choicePP("waifu".$i.".png", 200,200,20);
 	if($i%4 == 0){echo "<br>";}
 	
  }
+echo "<input class =\"subPP\" type=\"submit\"  value=\"Appliquer les changements\"";
+echo "</form>";
 }
 else { include_once "Forms/error.php";
 }
