a65ebea15898159d385478717865182f26e70635
Elliu <goyard.louis@gmail.com>
2020-05-09 19:57:35 +0200

    Now also displaying photo and adder in queue when logged in as non admin/root

--- public/Forms/getQueue.php
+++ public/Forms/getQueue.php
@@ -10,42 +10,38 @@ if ( isset($_GET["getQueue"]) && isset($_SESSION["id"]) )
 
     $dbAdapter = (new DbAdaperFactory())->createService();
 
+    $stmt =
+        'SELECT karas.source_name, karas.category, karas.song_number, karas.song_name, karas.id, "user".username, userCosmetics.IDimage
+         FROM queue JOIN karas
+            ON karas.id=queue.id
+            JOIN "user" ON queue.added_by="user".id
+            JOIN userCosmetics ON userCosmetics.id="user".id
+         ORDER BY queue.position ASC;';
+    $results = $dbAdapter->query($stmt)->fetchAll(PDO::FETCH_NUM);
+
     if ( $_SESSION['rights'] >= 1 ) // User is an admin or root
     {
-        $stmt =
-            'SELECT karas.source_name, karas.category, karas.song_number, karas.song_name, karas.id, "user".username, userCosmetics.IDimage
-             FROM queue JOIN karas
-                ON karas.id=queue.id
-                JOIN "user" ON queue.added_by="user".id
-                JOIN userCosmetics ON userCosmetics.id="user".id
-             ORDER BY queue.position ASC;';
-        $results = $dbAdapter->query($stmt)->fetchAll(PDO::FETCH_NUM);
-
         echo "<ul>";
         foreach ($results as $result)
         {
-            echo '<li><form method="POST">';
+            echo '<li>';
             viewPP("waifu$result[6].png", 50, 50, 5);
             echo $result[0] . " - " . $result[1] . $result[2] . " - " . $result[3] . " {" . $result[5] . "}";
-            echo '<button type="button" onclick="deleteKara(' . $result[4] . ')">Delete</button>
-                </form></li>';
+            echo '<button type="button" onclick="deleteKara(' . $result[4] . ')">Delete</button>';
+            echo '</li>';
         }
         echo "</ul>";
     }
 
     else // User is a peasant
     {
-        $stmt =
-            "SELECT karas.source_name, karas.category, karas.song_number, karas.song_name
-             FROM queue JOIN karas
-                ON karas.id=queue.id
-             ORDER BY queue.position ASC;";
-        $results = $dbAdapter->query($stmt)->fetchAll(PDO::FETCH_NUM);
-
         echo "<ul>";
         foreach ($results as $result)
         {
-            echo '<li>"' . $result[0] . " - " . $result[1] . $result[2] . " - " . $result[3] . '</li>';
+            echo '<li>';
+            viewPP("waifu$result[6].png", 50, 50, 5);
+            echo $result[0] . " - " . $result[1] . $result[2] . " - " . $result[3] . " {" . $result[5] . "}";
+            echo "</li>";
         }
         echo "</ul>";
     }
