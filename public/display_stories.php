93b63266538092912e4f60434ed3051fc01a6c0e
mael_berrou <mael.berrou@ensiie.fr>
2020-05-10 11:56:42 +0200

    commit, fixed display stories

--- public/display_stories.php
+++ public/display_stories.php
@@ -1,5 +1,12 @@
 <?php
 include_once '../src/utils/autoloader.php';
+use Story\StoryRepository;
 
-loadView('display_stories',[]);
+$db = (new DbAdaperFactory())->createService();
+$story_repo= new StoryRepository($db);
+$data=[];
+$stories=$story_repo->fetchAll();
+$data['stories']=$stories;
+
+loadView('display_stories',$data);
 ?>
--- src/View/display_stories.php
+++ src/View/display_stories.php
@@ -6,24 +6,13 @@
 
 <?php
 
-//connexion à la db
-$db = (new DbAdaperFactory())->createService();
-
-
-$query = $db->query("SELECT title FROM story");
-
-
-$title = $query->fetchAll();
-
 //title_story[0] correspond au titre de la story actuelle.
-foreach($title as $title_story) {
-	echo "Commencer l'histoire '$title_story[0]' en cliquant ";
-	echo '<a href="../../cite_des_voleurs.php"><strong>ici</strong></a>'; 
-	echo "<br />";
+foreach ($data['stories'] as $story) {
+	
+	echo'
+	<form method="get" action="story.php">
+	<input type="hidden" name="storyId" value="'.$story->getId().'"/>
+	<button type="submit" class="btn btn-link">'.$story->getTitle().'</button>
+</form>';
 }
 ?>
-
-<form method="get" action="story.php">
-	<input type="hidden" name="storyId" value="1"/>
-	<button type="submit" class="btn btn-link">La Cité des Voleurs</button>
-</form>
