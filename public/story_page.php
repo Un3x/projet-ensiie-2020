50409e5ea4f2d60d7094d22233490795c3d69ea6
mael_berrou <mael.berrou@ensiie.fr>
2020-05-10 19:20:48 +0200

    ajout de l'affichage de la note de chaque histoire (pas encore completement fonctionnel)

--- public/story_page.php
+++ public/story_page.php
@@ -2,17 +2,38 @@
 include_once '../src/utils/autoloader.php';
 
 use Story\StoryRepository;
+use Rating\RatingRepository;
 
 $dbAdaper = (new DbAdaperFactory())->createService();
 $storyRepo = new Story\StoryRepository($dbAdaper);
+$db = (new DbAdaperFactory())->createService();
+$ratingRepo = new Rating\RatingRepository($db);
 $data =[];
+
 
 if (isset($_GET['storyId'])) {
   // Get story data
   $story =  $storyRepo->fetchStory($_GET['storyId']);
   $data['story']=$story;
   
-  // Get rating data ... I guess
+  // Get rating data 
+  $rating = $ratingRepo->fetchRating($_GET['storyId']);
+  $data['rating']=$rating;
+  $avg_rate=$ratingRepo->fetchAvgRating($data['story']->getId());
+  $data['avg_rate']=$avg_rate;
+  $count_rates=$ratingRepo->fetchCountRating($data['story']->getId());
+  $data['count']=$count_rates;
+  $star1=$ratingRepo->fetchStarCount($data['story']->getId(),1);
+  $data['star1']=$star1;
+  $star2=$ratingRepo->fetchStarCount($data['story']->getId(),2);
+  $data['star2']=$star2;
+  $star3=$ratingRepo->fetchStarCount($data['story']->getId(),3);
+  $data['star3']=$star3;
+  $star4=$ratingRepo->fetchStarCount($data['story']->getId(),4);
+  $data['star4']=$star4;
+  $star5=$ratingRepo->fetchStarCount($data['story']->getId(),5);
+  $data['star5']=$star5;
+
 
   // Get comment data ... XD lol
 
--- src/View/story_page.php
+++ src/View/story_page.php
@@ -10,3 +10,74 @@
 	<input type="hidden" name="storyId" value="<?php echo $data['story']->getId(); ?>"/>
 	<button type="submit" class="btn btn-primary">Commencer</button>
 </form>
+
+
+ <!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<span class="heading">User Rating</span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span>
+<p> <?php echo $data['avg_rate'] ?> en moyenne, basé sur <?php echo $data['count'] ?> notes.</p>
+<hr style="border:3px solid #f1f1f1">
+
+<div class="row">
+  <div class="side">
+    <div>5 star</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar-5"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div><?php echo $data['star5'] ?></div>
+  </div>
+  <div class="side">
+    <div>4 star</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar-4"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div><?php echo $data['star4'] ?></div>
+  </div>
+  <div class="side">
+    <div>3 star</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar-3"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div><?php echo $data['star3'] ?></div>
+  </div>
+  <div class="side">
+    <div>2 star</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar-2"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div><?php echo $data['star2'] ?></div>
+  </div>
+  <div class="side">
+    <div>1 star</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar-1"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div><?php echo $data['star1'] ?></div>
+  </div>
+</div> 
