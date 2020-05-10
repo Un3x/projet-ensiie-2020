f9d21699f1679f6ac37a67a096f2ea531335bc19
Elliu <goyard.louis@gmail.com>
2020-05-10 15:29:34 +0200

    Finishing header css

--- src/View/Layout/header.php
+++ src/View/Layout/header.php
@@ -4,25 +4,26 @@ session_start();
 
 <header>
     <ul class="topnav">
-        <li><a class="site-name" href="#">Bakaraoke</a></li>
-        <li><a class="nav-link" href="search.php">Advanced Search</a></li>
+        <li class="left-side"><a class="site-name" href="#">Bakaraoke</a></li>
+        <li class="left-side"><a class="nav-link" href="search.php">Advanced Search</a></li>
     <?php if ( isset($_SESSION['rights']) && ( $_SESSION['rights']===1 || $_SESSION['rights']===2 ) ) { ?>
-        <li><a class="nav-link" href="admin.php">Manage Users</a></li>
+        <li class="left-side"><a class="nav-link" href="admin.php">Manage Users</a></li>
     <?php } ?>
-        <li><a class="nav-link" href="modifyUser.php">Edit your profile</a></li>
+        <li class="left-side"><a class="nav-link" href="modifyUser.php">Edit your profile</a></li>
         <li class="login-feedback">
         <?php
         if (isset($_SESSION['username']))
         {
             $idSession=$_SESSION['id'];
             $userSession=$_SESSION['username'];
-            echo "You are logged in as $userSession, $idSession\n";
+            echo "You are logged in as $userSession. ";
+            echo '<a href="Forms/logout.php">Logout</a> / <a href="modifyUser.php">Settings</a>';
         }
         else
         {
             echo "You are logged out : <a class='nav-link' href='login.php'>Login</a>\n";
         }
         ?>
-        </li>
     </ul>
+        </li>
 </header>
