c915c98057fdd33bc4cb9f29b2b787c1ae6602d6
Elliu <goyard.louis@gmail.com>
2020-05-10 23:32:18 +0200

    Minor changes to createPlaylist

--- src/View/Layout/createPlaylist.php
+++ src/View/Layout/createPlaylist.php
@@ -1,18 +1,13 @@
-<!-- cette page permet de creer un compte 
-envoie les informations à Forms/addUser
--->
-
-<!--creer un compte -->
-<h1>Nommez votre playlist:</h1>
+<h1>Create your playlist:</h1>
 <form name= "formAddPlaylist" action="Forms/addPlaylist.php" onsubmit="return validCreatePlaylist();" method="POST">
-  <label for="name"> Nom de playlist : </label>
-  <input type="text" name="name" placeholder="name" maxlength="32"></br>  
-
-Publique ? :
-<input type="radio" name="publik" <?php if (isset($publik) && $publik="TRUE") echo "checked"; ?> value="TRUE">Oui 
-<input type="radio" name="publik" <?php if (isset($publik) && $publik="FALSE") echo "checked"; ?> value="FALSE">Non
-</br>
-<button type="submit">Creer la playlist</button>
-</br>
+    <ul>
+        <li><label for="name">Playlist name: </label>
+        <input type="text" name="name" placeholder="name" maxlength="32"></li>
+        <li>Public:
+        <input type="radio" name="publik" value="TRUE">Yes</input>
+        <input type="radio" name="publik" value="FALSE" checked>No</input></li>
+        <li><button type="submit">Create playlist</button></li>
+    </ul>
+</form>
 
 <script type="text/javascript" src="scripts/formulaire.js"></script>
