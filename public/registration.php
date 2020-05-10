6aa3caa9f2cbb2ca569412701b073ec85562d2e1
Kiwi5 <kiwi@LAPTOP-D4ADFV9F.localdomain>
2020-05-10 22:30:10 +0200

    header added on login and registration

--- public/registration.php
+++ public/registration.php
@@ -5,6 +5,7 @@ set_include_path('.:' . $_SERVER['DOCUMENT_ROOT'] . '/../src');
 
 <?php include_once "View/Layout/head.php" ?>
 </head>
+<?php include_once "View/Layout/header.php"?>
 
 <body>
 <?php include_once "View/Layout/registration.php" ?>
