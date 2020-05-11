e72ddeaefe319049052fcab28b1582873313619b
Quentin LECOMTE <quentin.lecomte@ensiie.fr>
2020-05-11 13:42:59 +0200

    Encore correction d'un souci

--- src/Repository/SaveRepository.php
+++ src/Repository/SaveRepository.php
@@ -27,7 +27,7 @@ class SaveRepository
 
     public function fetchAll(int $userId)
     {
-        $savesData = $this->dbAdapter->prepare('SELECT saveId, userId, title, storyId, pageId, saves.skill, saves.stamina, saves.luck FROM saves NATURAL JOIN story WHERE userId=:userid');
+        $savesData = $this->dbAdapter->prepare('SELECT saveId, userId, title, storyId, pageId, saves.skill, saves.stamina, saves.luck FROM saves JOIN story ON title == storyName');
         $savesData->bindParam(':userid', $userId);
         $savesData->execute();
         $saves = [];
