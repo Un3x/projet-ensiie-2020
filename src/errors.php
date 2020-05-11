c35f9f9a61c932c1d217cf304d822d3ce48cbc9c
Elliu <goyard.louis@gmail.com>
2020-05-11 12:04:05 +0200

    Improving errors.php

--- src/errors.php
+++ src/errors.php
@@ -3,6 +3,7 @@
 function connect_yourself()
 {
     echo '<p>Ara Ara, you look like a fine boy. Too bad you aren\'t <a href="/login.php">connected</a>.</p>';
+    echo '<p><img src="/images/ara_ara.jpg"</p>';
 }
 
 ?>
